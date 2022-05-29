800a77e96932822b5010eef8a7c09cb31be87fc2
overdose420 <skeptic.ng@gmail.com>
2022-05-29 19:42:29 +0700

    first commit

---
+++ main_test.go
@@ -0,0 +1,173 @@
+package main_test
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/cenkalti/backoff/v4"
+	"github.com/cockroachdb/cockroach-go/v2/crdb"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	db, err := initStore()
+	if err != nil {
+		log.Fatalf("failed to initialise the store: %s", err)
+	}
+	defer db.Close()
+
+	e.GET("/", func(c echo.Context) error {
+		return rootHandler(db, c)
+	})
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+
+	e.POST("/send", func(c echo.Context) error {
+		return sendHandler(db, c)
+	})
+
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+}
+
+type Message struct {
+	Value string `json:"value"`
+}
+
+func initStore() (*sql.DB, error) {
+
+	pgConnString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGDATABASE"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+	)
+
+	var (
+		db  *sql.DB
+		err error
+	)
+	openDB := func() error {
+		db, err = sql.Open("postgres", pgConnString)
+		return err
+	}
+
+	err = backoff.Retry(openDB, backoff.NewExponentialBackOff())
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := db.Exec(
+		"CREATE TABLE IF NOT EXISTS message (value STRING PRIMARY KEY)"); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func rootHandler(db *sql.DB, c echo.Context) error {
+	r, err := countRecords(db)
+	if err != nil {
+		return c.HTML(http.StatusInternalServerError, err.Error())
+	}
+	return c.HTML(http.StatusOK, fmt.Sprintf("Hello, Docker! (%d)\n", r))
+}
+
+func sendHandler(db *sql.DB, c echo.Context) error {
+
+	m := &Message{}
+
+	if err := c.Bind(m); err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	err := crdb.ExecuteTx(context.Background(), db, nil,
+		func(tx *sql.Tx) error {
+			_, err := tx.Exec(
+				"INSERT INTO message (value) VALUES ($1) ON CONFLICT (value) DO UPDATE SET value = excluded.value",
+				m.Value,
+			)
+			if err != nil {
+				return c.JSON(http.StatusInternalServerError, err)
+			}
+			return nil
+		})
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, m)
+}
+
+func countRecords(db *sql.DB) (int, error) {
+
+	rows, err := db.Query("SELECT COUNT(*) FROM message")
+	if err != nil {
+		return 0, err
+	}
+	defer rows.Close()
+
+	count := 0
+	for rows.Next() {
+		if err := rows.Scan(&count); err != nil {
+			return 0, err
+		}
+		rows.Close()
+	}
+
+	return count, nil
+}
+
+func TestRespondsWithLove(t *testing.T) {
+
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err, "could not connect to Docker")
+
+	resource, err := pool.Run("docker-gs-ping", "latest", []string{})
+	require.NoError(t, err, "could not start container")
+
+	t.Cleanup(func() {
+		require.NoError(t, pool.Purge(resource), "failed to remove container")
+	})
+
+	var resp *http.Response
+
+	err = pool.Retry(func() error {
+		resp, err = http.Get(fmt.Sprint("http://localhost:", resource.GetPort("8080/tcp"), "/"))
+		if err != nil {
+			t.Log("container not ready, waiting...")
+			return err
+		}
+		return nil
+	})
+	require.NoError(t, err, "HTTP error")
+	defer resp.Body.Close()
+
+	require.Equal(t, http.StatusOK, resp.StatusCode, "HTTP status code")
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err, "failed to read HTTP body")
+
+	// Finally, test the business requirement!
+	require.Contains(t, string(body), "<3", "does not respond with love?")
+}
