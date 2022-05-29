6af1141ad8fd3a35be5e2bdedd3d10ca988567be
overdose420 <skeptic.ng@gmail.com>
2022-05-29 20:19:16 +0700

    add maintest

--- main_test.go
+++ main_test.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"testing"
 
 	"github.com/cenkalti/backoff/v4"
 	"github.com/cockroachdb/cockroach-go/v2/crdb"
@@ -136,38 +135,4 @@ func countRecords(db *sql.DB) (int, error) {
 	}
 
 	return count, nil
-}
-
-func TestRespondsWithLove(t *testing.T) {
-
-	pool, err := dockertest.NewPool("")
-	require.NoError(t, err, "could not connect to Docker")
-
-	resource, err := pool.Run("docker-gs-ping", "latest", []string{})
-	require.NoError(t, err, "could not start container")
-
-	t.Cleanup(func() {
-		require.NoError(t, pool.Purge(resource), "failed to remove container")
-	})
-
-	var resp *http.Response
-
-	err = pool.Retry(func() error {
-		resp, err = http.Get(fmt.Sprint("http://localhost:", resource.GetPort("8080/tcp"), "/"))
-		if err != nil {
-			t.Log("container not ready, waiting...")
-			return err
-		}
-		return nil
-	})
-	require.NoError(t, err, "HTTP error")
-	defer resp.Body.Close()
-
-	require.Equal(t, http.StatusOK, resp.StatusCode, "HTTP status code")
-
-	body, err := io.ReadAll(resp.Body)
-	require.NoError(t, err, "failed to read HTTP body")
-
-	// Finally, test the business requirement!
-	require.Contains(t, string(body), "<3", "does not respond with love?")
 }
